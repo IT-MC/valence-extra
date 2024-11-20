a31737ff0ad73e839174437941ebad504df25dc9
regalk <72028266+regalk13@users.noreply.github.com>
2024-11-20 08:46:37 -0500

    feat: add match to Compound for Schematic values

--- crates/schematics/src/decode.rs
+++ crates/schematics/src/decode.rs
@@ -4,7 +4,7 @@ use crate::{Schematic, SchematicError};
 
 impl Schematic {
     pub fn from_bytes(mut bytes: &[u8]) -> Result<Self, SchematicError> {
-        let (nbt, root_name) = if &bytes.len() >= &2 && &bytes[0..2] == [0x1F, 0x8B] {
+        let (nbt, _) = if &bytes.len() >= &2 && &bytes[0..2] == [0x1F, 0x8B] {
             let mut new_bytes = flate2::read::GzDecoder::new(bytes);
             let mut buffer = Vec::new();
             let _ = new_bytes
@@ -19,8 +19,22 @@ impl Schematic {
             valence_nbt::from_binary::<String>(&mut bytes)
                 .map_err(|_| SchematicError::ParseError(String::new()))?
         };
-        println!("NBT: {:?}", nbt);
-        println!("Root Name: {:?}", root_name);
+
+        let schematic_value = nbt.get("Schematic");
+        if let Some(valence_nbt::Value::Compound(compound)) = schematic_value {
+            if let Some(width) = compound.get("Width") {
+                if let valence_nbt::Value::Short(w) = width {
+                    println!("Width: {}", w);
+                }
+            } else {
+                SchematicError::InvalidFormat(String::new());
+            }
+        } else {
+            SchematicError::InvalidFormat(String::new());
+        }
+
+        //  println!("NBT: {:?}", schematic_result);
+        //  println!("Root Name: {:?}", root_name);
         Ok(Schematic { w: 0.0, h: 0.0 })
     }
 
--- examples/schematics.rs
+++ examples/schematics.rs
@@ -10,7 +10,7 @@ pub fn main() {
 
 fn setup() {
     // flate version
-    let _ = Schematic::from_bytes(include_bytes!("./assets/unzipcabaret.schem")).unwrap();
+    // let _ = Schematic::from_bytes(include_bytes!("./assets/unzipcabaret.schem")).unwrap();
 
     // deflate version
     let _ = Schematic::from_bytes(include_bytes!("./assets/cabaret-t5.schem")).unwrap();
