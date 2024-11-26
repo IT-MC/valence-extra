42a101ce2488912989687a75c384c2d2e9ed42d8
regalk <72028266+regalk13@users.noreply.github.com>
2024-11-26 17:21:41 -0500

    feat: add all BlockEntity types

--- crates/schematics/src/block_entity_data.rs
+++ crates/schematics/src/block_entity_data.rs
@@ -33,49 +33,49 @@ pub enum BlockEntityData {
     #[serde(rename = "minecraft:command_block")]
     CommandBlock(CommandBlockEntityData),
     #[serde(rename = "minecraft:conduit")]
-    Conduit(DefaultBlockEntityData),
+    Conduit(ConduitBlockEntityData),
     #[serde(rename = "minecraft:daylight_detector")]
     DayLightDetector(DefaultBlockEntityData),
     #[serde(rename = "minecraft:decorated_pot")]
     DecoratedPot(DefaultBlockEntityData),
     #[serde(rename = "minecraft:dispenser")]
-    Dispenser(DefaultBlockEntityData),
+    Dispenser(DispenserBlockEntityData),
     #[serde(rename = "minecraft:dropper")]
-    Dropper(DefaultBlockEntityData),
+    Dropper(DropperBlockEntityData),
     #[serde(rename = "minecraft:enchanting_table")]
     EnchantingTable(DefaultBlockEntityData),
     #[serde(rename = "minecraft:ender_chest")]
     EnderChest(DefaultBlockEntityData),
     #[serde(rename = "minecraft:end_gateway")]
-    EndGateway(DefaultBlockEntityData),
+    EndGateway(EndGatewayBlockEntityData),
     #[serde(rename = "minecraft:end_portal")]
     EndPortal(DefaultBlockEntityData),
-    #[serde(rename = "minecraft:furnance")]
-    Furnance(DefaultBlockEntityData),
+    #[serde(rename = "minecraft:furnace")]
+    Furnace(FurnaceBlockEntityData),
     #[serde(rename = "minecraft:hopper")]
-    Hopper(DefaultBlockEntityData),
+    Hopper(HopperBlockEntityData),
     #[serde(rename = "minecraft:jigsaw")]
-    JigSaw(DefaultBlockEntityData),
+    JigSaw(JigSawBlockEntityData),
     #[serde(rename = "minecraft:jukebox")]
-    JukeBox(DefaultBlockEntityData),
+    JukeBox(JukeBoxBlockEntityData),
     #[serde(rename = "minecraft:lectern")]
-    Lectern(DefaultBlockEntityData),
+    Lectern(LecternBlockEntityData),
     #[serde(rename = "minecraft:mob_spawner")]
     MobSpawner(DefaultBlockEntityData),
     #[serde(rename = "minecraft:piston")]
-    Piston(DefaultBlockEntityData),
+    Piston(PistonBlockEntityData),
     #[serde(rename = "minecraft:shulker_box")]
-    ShulkerBox(DefaultBlockEntityData),
+    ShulkerBox(ShulkerBoxBlockEntityData),
     #[serde(rename = "minecraft:skull")]
-    Skull(DefaultBlockEntityData),
+    Skull(SkullBlockEntityData),
     #[serde(rename = "minecraft:smoker")]
-    Smoker(DefaultBlockEntityData),
+    Smoker(SmokerBlockEntityData),
     #[serde(rename = "minecraft:soul_campfire")]
-    SoulCampFire(DefaultBlockEntityData),
+    SoulCampFire(SoulCampFireBlockEntityData),
     #[serde(rename = "minecraft:structure_block")]
-    StructureBlock(DefaultBlockEntityData),
+    StructureBlock(Map<String, serde_json::Value>),
     #[serde(rename = "minecraft:trapped_chest")]
-    TrappedChest(DefaultBlockEntityData),
+    TrappedChest(TrappedChestBlockEntityData),
 }
 
 // Implement Struct of each BlockEntityData
@@ -224,4 +224,206 @@ pub struct ComparatorBlockEntityData {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct CommandBlockEntityData;
+pub struct CommandBlockEntityData {
+    pub auto: i8,
+    #[serde(rename = "Command")]
+    pub command: String,
+    #[serde(rename = "conditionMet")]
+    pub condition_met: i8,
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "LastExecution")]
+    pub last_execution: i64,
+    #[serde(rename = "LastOutput")]
+    pub last_output: String,
+    pub powered: i8,
+    #[serde(rename = "SuccessCount")]
+    pub success_count: i32,
+    #[serde(rename = "TrackOutput")]
+    pub track_output: i8,
+    #[serde(rename = "UpdateLastExecution")]
+    pub update_last_execution: i8,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ConduitBlockEntityData {
+    #[serde(rename = "Target")]
+    pub target: Vec<i32>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DispenserBlockEntityData {
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+    #[serde(rename = "Lock")]
+    pub lock: Option<String>,
+    #[serde(rename = "LootTable")]
+    pub loot_table: Option<String>,
+    #[serde(rename = "LootTableSeed")]
+    pub loot_table_seed: Option<i64>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct DropperBlockEntityData {
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+    #[serde(rename = "Lock")]
+    pub lock: Option<String>,
+    #[serde(rename = "LootTable")]
+    pub loot_table: Option<String>,
+    #[serde(rename = "LootTableSeed")]
+    pub loot_table_seed: Option<i64>,
+    #[serde(rename = "Lunar")]
+    pub lunar: i8,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct EndGatewayBlockEntityData {
+    #[serde(rename = "Age")]
+    pub age: i64,
+    #[serde(rename = "ExactTeleport")]
+    pub extract_teleport: i8,
+    #[serde(rename = "ExitPortal")]
+    pub exit_portal: Map<String, serde_json::Value>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct FurnaceBlockEntityData {
+    #[serde(rename = "BurnTime")]
+    pub burn_time: i16,
+    #[serde(rename = "CookTime")]
+    pub cook_time: i16,
+    #[serde(rename = "CookTimeTotal")]
+    pub cook_time_total: i16,
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+    #[serde(rename = "Lock")]
+    pub lock: Option<String>,
+    #[serde(rename = "RecipesUsed")]
+    pub recipes_used: Map<String, serde_json::Value>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct HopperBlockEntityData {
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+    #[serde(rename = "Lock")]
+    pub lock: Option<String>,
+    #[serde(rename = "LootTable")]
+    pub loot_table: Option<String>,
+    #[serde(rename = "LootTableSeed")]
+    pub loot_table_seed: Option<i64>,
+    #[serde(rename = "TransferCooldown")]
+    pub transfer_cooldown: i32,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct JigSawBlockEntityData {
+    pub final_state: String,
+    pub joint: Option<String>,
+    pub name: String,
+    pub pool: String,
+    pub target: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct JukeBoxBlockEntityData {
+    #[serde(rename = "IsPlaying")]
+    pub is_playing: i8,
+    #[serde(rename = "RecordItem")]
+    pub record_item: Map<String, serde_json::Value>,
+    #[serde(rename = "RecordStartTick")]
+    pub record_start_tick: i64,
+    #[serde(rename = "TickCount")]
+    pub tick_count: i64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct LecternBlockEntityData {
+    #[serde(rename = "Book")]
+    pub book: Map<String, serde_json::Value>,
+    #[serde(rename = "Page")]
+    pub page: i32,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PistonBlockEntityData {
+    #[serde(rename = "blockState")]
+    pub block_state: Map<String, serde_json::Value>,
+    pub extending: i8,
+    pub facing: i32,
+    pub progress: f32,
+    pub source: i8,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ShulkerBoxBlockEntityData {
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+    #[serde(rename = "Lock")]
+    pub lock: Option<String>,
+    #[serde(rename = "LootTable")]
+    pub loot_table: Option<String>,
+    #[serde(rename = "LootTableSeed")]
+    pub loot_table_seed: Option<i64>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SkullBlockEntityData {
+    pub note_block_sound: Option<String>,
+    pub extra_type: Option<String>,
+    pub skull_owner: Map<String, serde_json::Value>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SmokerBlockEntityData {
+    #[serde(rename = "BurnTime")]
+    pub burn_time: i16,
+    #[serde(rename = "CookTime")]
+    pub cook_time: i16,
+    #[serde(rename = "CookTimeTotal")]
+    pub cook_time_total: i16,
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+    #[serde(rename = "Lock")]
+    pub lock: Option<String>,
+    #[serde(rename = "RecipesUsed")]
+    pub recipes_used: Map<String, serde_json::Value>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SoulCampFireBlockEntityData {
+    #[serde(rename = "CookingTimes")]
+    pub cooking_times: Vec<i32>,
+    #[serde(rename = "CookingTotalTimes")]
+    pub cooking_total_times: Vec<i32>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TrappedChestBlockEntityData {
+    #[serde(rename = "CustomName")]
+    pub custom_name: Option<String>,
+    #[serde(rename = "Items")]
+    pub items: Vec<Item>,
+    #[serde(rename = "Lock")]
+    pub lock: Option<String>,
+    #[serde(rename = "LootTable")]
+    pub loot_table: Option<String>,
+    #[serde(rename = "LootTableSeed")]
+    pub loot_table_seed: Option<i64>,
+    pub gold: Option<i8>,
+}
